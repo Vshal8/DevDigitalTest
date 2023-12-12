cd6e7f22f8754bf5b6552e84151581b1b975d6d2
Vshal8 <vshalattri@gmail.com>
2023-12-12 15:18:03 +0530

    Started Test

---
+++ App.js
@@ -0,0 +1,34 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+function App() {
+  return (
+    <SafeAreaView style={{flex:1}}>
+
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+ 
+});
+
+export default App;
