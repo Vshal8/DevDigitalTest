5351b533e507302469e3c985d990f9b99057d0a1
Vshal8 <vshalattri@gmail.com>
2023-12-12 16:38:55 +0530

    Added timer functionality

--- src/components/CountdownListItem.js
+++ src/components/CountdownListItem.js
@@ -1,16 +1,35 @@
-import React, { memo, useState } from "react";
+import React, { memo, useCallback, useEffect, useState } from "react";
 import { StyleSheet, View } from "react-native";
 import { colors } from "../utils/theme";
 import CommonButton from "./CommonButton";
 import TimerComponent from "./TimerComponent";
 
-
 function CountdownListItem({ item, index }) {
-
+    const [isStarted, setIsStarted] = useState(false)
     const [remainingTime, setRemainingTime] = useState(item.time)
 
-    function onButtonPress() {
 
+    let interval;
+    useEffect(() => {
+        if (isStarted) {
+            if (remainingTime > 0) {
+                interval = setTimeout(() => {
+                    let updatedTime = remainingTime
+                    updatedTime = updatedTime - 1
+                    setRemainingTime(updatedTime)
+                }, 1000)
+            }
+            else {
+                setIsStarted(false)
+            }
+        }
+        else {
+            clearInterval(interval)
+        }
+    }, [isStarted, remainingTime])
+
+    function onButtonPress() {
+        setIsStarted(!isStarted)
     }
 
     return (
@@ -22,7 +41,7 @@ function CountdownListItem({ item, index }) {
                 <TimerComponent time={remainingTime} isFormatted={true} />
             </View>
             <View style={styles.buttonContainer}>
-                <CommonButton buttonTitle={'Start'} onButtonPress={onButtonPress} customButtonStyles={styles.buttonStyles}/>
+                <CommonButton buttonTitle={isStarted ? 'Pause' : 'Start'} onButtonPress={onButtonPress} customButtonStyles={styles.buttonStyles} />
             </View>
         </View>
     )
@@ -51,8 +70,8 @@ const styles = StyleSheet.create({
         flex: 1,
         marginLeft: 10
     },
-    buttonStyles:{
-        padding:5,
-        borderRadius:12
+    buttonStyles: {
+        padding: 5,
+        borderRadius: 12
     }
 })
